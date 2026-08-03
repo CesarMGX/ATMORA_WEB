8360e64d285ef109d38dfab654bd633ac3bce8fe
CesarMGX <startcrazy21@gmail.com>
2026-08-02 22:51:46 -0600

    feat: Redirigir los enlaces de Inicio de sesión y Registro y que aparezca la animación de celebración en el enlace de Descarga de la app desde el footer

--- src/app/features/landing/components/footer/footer.ts
+++ src/app/features/landing/components/footer/footer.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CelebrationService } from '../../../../core/services/celebration.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
+  constructor(private celebrationService: CelebrationService) {}
 
+  descargarApp(event: Event) {
+    event.preventDefault();
+    this.celebrationService.mostrarCelebracion();
+  }
 }
+
